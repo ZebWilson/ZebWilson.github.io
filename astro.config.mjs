7cb0b4877a5230faad0be2225dd7892599ceaefc
zebz13 <zeb@fedora>
2025-10-20 19:44:55 +0530

    Trying out astro default blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
+});
---
+++ src/content/blog/second-post.md
@@ -0,0 +1,12 @@
+---
+title: 'Second post'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: 'Jul 15 2022'
+heroImage: '../../assets/blog-placeholder-4.jpg'
+---
+
+Testing with equations to see how it goes
+
+$$
+h^2
+$$
